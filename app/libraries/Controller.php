16b60758b7f2f0465dfcb053ede8d92fae3c10a0
ChehAchraf <achrafchehboun@outlook.com>
2025-02-10 11:48:06 +0100

    init the porject and add some files to it

---
+++ app/libraries/Controller.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Libraries;
+
+// use App\Libraries\Database;
+
+class Controller {
+    protected $db;
+
+    public function __construct() {
+        // 3yt 3La db
+        $this->db = new Database;
+    }
+
+    // charger model
+    public function model($model){
+      // lassa9 model file
+      require_once '../app/models/' . $model . '.php';
+
+      // Instatiate model
+      $className = 'App\\Models\\' . $model;
+      return new $className();
+    }
+
+    // Load view
+    public function view($view, $data = []){
+      // Check for view file
+      if(file_exists('../app/views/' . $view . '.php')){
+        require_once '../app/views/' . $view . '.php';
+      } else {
+        // View does not exist
+        die('View does not exist');
+      }
+    }
+  }
