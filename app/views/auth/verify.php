6143065db4a9f1ac6e77880a830b5299b8aea06d
ChehAchraf <achrafchehboun@outlook.com>
2025-02-14 15:02:26 +0100

    add the layout

--- app/views/auth/verify.php
+++ app/views/auth/verify.php
@@ -1,5 +1,8 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 
+<div class="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+
+</div>
 
 
 <?php require APPROOT . '/views/inc/footer.php'; ?> 
