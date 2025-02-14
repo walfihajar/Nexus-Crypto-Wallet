3e9f8103b529ab5d4955a5fb268f21c4f38f4356
ChehAchraf <achrafchehboun@outlook.com>
2025-02-14 15:04:03 +0100

    fix the display error

--- app/views/auth/verify.php
+++ app/views/auth/verify.php
@@ -1,7 +1,7 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 
 <div class="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-<div class="max-w-md w-full space-y-8 bg-gray-800 p-8 rounded-xl">
+    <div class="max-w-md w-full space-y-8 bg-gray-800 p-8 rounded-xl">
         <div>
             <h2 class="mt-6 text-center text-3xl font-extrabold text-white">
                 Verify Your Account
