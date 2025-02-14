9afc85f782f3e54a4f2537b7c413c6040f22b648
ChehAchraf <achrafchehboun@outlook.com>
2025-02-14 13:09:23 +0100

    add the validation for email

--- app/models/User.php
+++ app/models/User.php
@@ -14,4 +14,25 @@ class User {
         return true;
     }
 
+    protected function validateSignup(array $data): bool {
+        if(empty($data['firstname']) || empty($data['lastname']) || 
+           empty($data['email']) || empty($data['password']) || 
+           empty($data['birth_date'])) {
+            return false;
+        }
+
+        if(!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+            return false;
+        }
+
+        if(strlen($data['password']) < 6) {
+            return false;
+        }
+
+        if($this->emailExists($data['email'])) {
+            return false;
+        }
+
+        return true;
+    }
 }
