933f311290108338568566dd8924f8f811c0abad
ChehAchraf <achrafchehboun@outlook.com>
2025-02-14 13:10:04 +0100

    add the functions that handle the password

--- app/models/User.php
+++ app/models/User.php
@@ -35,4 +35,12 @@ class User {
 
         return true;
     }
+
+    protected function hashPassword(string $password): string {
+        return password_hash($password, PASSWORD_DEFAULT);
+    }
+
+    protected function verifyPassword(string $password, string $hash): bool {
+        return password_verify($password, $hash);
+    }
 }
