645cf102c92585afbf9e1aa6cff1ee32c3817c84
ChehAchraf <achrafchehboun@outlook.com>
2025-02-14 14:56:13 +0100

    add the logout function

--- app/controllers/Auth.php
+++ app/controllers/Auth.php
@@ -228,4 +228,12 @@ class Auth extends Controller{
 
         redirect('auth/verify');
     }
+
+    public function logout() {
+        unset($_SESSION['user_id']);
+        unset($_SESSION['user_email']);
+        unset($_SESSION['user_name']);
+        session_destroy();
+        redirect('auth/login');
+    }
 }
